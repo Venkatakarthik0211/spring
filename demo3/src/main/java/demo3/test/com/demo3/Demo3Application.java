ac2a385c316c2931ac92c09b9e8e13d89e7a5907
ss360 <kdraksharam@yahoo.com>
2024-05-20 01:48:20 +0530

    logger statement commented

--- demo3/src/main/java/demo3/test/com/demo3/Demo3Application.java
+++ demo3/src/main/java/demo3/test/com/demo3/Demo3Application.java
@@ -16,7 +16,7 @@ public class Demo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo3Application.class, args);
-		final Logger logger = LoggerFactory.getLogger(Demo3Application.class);
+		//final Logger logger = LoggerFactory.getLogger(Demo3Application.class);
 	}
 
 	@Bean
